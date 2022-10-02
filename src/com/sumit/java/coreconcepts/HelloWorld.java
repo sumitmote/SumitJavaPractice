cc1f4dd4b4d9bf037d3c3fdbec46cc3fc8298306
sumitmote <sumitmote19@gmail.com>
2022-10-02 19:41:58 +0530

    initial commit

---
+++ src/com/sumit/java/coreconcepts/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.sumit.java.coreconcepts;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, This is hello world called.");
+    }
+
+}
